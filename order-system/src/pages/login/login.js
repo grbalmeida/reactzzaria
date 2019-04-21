b0e8596d50ed30cc3fe6f278a549c5734350d376
gilvan <g.r.almeida@live.com>
2019-04-21 01:44:20 -0300

    Adding markup to login screen

--- order-system/src/pages/login/login.js
+++ order-system/src/pages/login/login.js
@@ -1,7 +1,12 @@
-import React from 'react'
+import React, { Fragment } from 'react'
+import { Button } from '@material-ui/core'
+import { ReactComponent as Logo } from './logo.svg'
 
 const Login = () => (
-  <h1>Login</h1>
+  <Fragment>
+    <Logo />
+    <Button>Log in with GitHub</Button>
+  </Fragment>
 )
 
 export default Login
