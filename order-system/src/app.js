2a000b7f8e75aabf867b9ad0fa8edb21dc00c572
gilvan <g.r.almeida@live.com>
2019-04-21 17:21:10 -0300

    Adding user data in the Context API (app)

--- order-system/src/app.js
+++ order-system/src/app.js
@@ -1,11 +1,26 @@
-import React, { lazy, Suspense } from 'react'
+import React, { lazy, useContext, useEffect, Suspense } from 'react'
 import { Switch, Route } from 'react-router-dom'
 import { LinearProgress } from '@material-ui/core'
+import firebase from 'services/firebase'
+import { AuthContext } from 'contexts/auth'
 
 const MainPage = lazy(() => import('pages/main'))
 const Login = lazy(() => import('pages/login'))
 
 function App () {
+  const { userInfo, setUserInfo } = useContext(AuthContext)
+
+  useEffect(() => {
+    firebase.auth().onAuthStateChanged((user) => {
+      setUserInfo({
+        isUserLoggedIn: !!user,
+        user,
+        userInfo,
+        setUserInfo
+      })
+    })
+  }, [])
+
   return (
     <Suspense fallback={<LinearProgress />}>
       <Switch>
--- order-system/src/pages/login/login.js
+++ order-system/src/pages/login/login.js
@@ -1,44 +1,11 @@
-import React, {
-  useState,
-  useEffect,
-  useCallback,
-  useContext,
-  Fragment
-} from 'react'
+import React, { useContext } from 'react'
 import styled from 'styled-components'
 import { Button, Grid } from '@material-ui/core'
-import firebase from 'services/firebase'
 import { AuthContext } from 'contexts/auth'
 import { ReactComponent as MainLogo } from './logo.svg'
 
 function Login () {
   const { login } = useContext(AuthContext)
-
-  const [userInfo, setUserInfo] = useState({
-    isUserLoggedIn: false,
-    user: null
-  })
-
-  const logout = useCallback(() => {
-    firebase.auth().signOut().then(() => {
-      console.log('Logged out!')
-      setUserInfo({
-        isUserLoggedIn: false,
-        user: null
-      })
-    })
-  }, [])
-
-  const { isUserLoggedIn, user } = userInfo
-
-  useEffect(() => {
-    firebase.auth().onAuthStateChanged((user) => {
-      setUserInfo({
-        isUserLoggedIn: !!user,
-        user
-      })
-    })
-  }, [])
 
   return (
     <Container>
@@ -47,15 +14,7 @@ function Login () {
           <Logo />
         </Grid>
         <Grid item xs={12} container justify='center'>
-          {isUserLoggedIn && (
-            <Fragment>
-              <pre>{JSON.stringify(user.displayName, null, 2)}</pre>
-              <Button variant='contained' onClick={logout}>Sair</Button>
-            </Fragment>
-          )}
-          {!isUserLoggedIn && (
-            <GitHubButton onClick={login}>Log in with GitHub</GitHubButton>
-          )}
+          <GitHubButton onClick={login}>Log in with GitHub</GitHubButton>
         </Grid>
       </Grid>
     </Container>
