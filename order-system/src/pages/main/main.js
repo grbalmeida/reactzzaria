891942ac9ae7a939c426ae79eb19376a59808f16
gilvan <g.r.almeida@live.com>
2019-04-21 01:08:13 -0300

    Internal routes in MainPage

--- order-system/src/pages/main/main.js
+++ order-system/src/pages/main/main.js
@@ -1,7 +1,25 @@
-import React from 'react'
+import React, { Fragment } from 'react'
+import { Route, Switch } from 'react-router-dom'
+
+const routes = [
+  { path: '/route1', content: 'Route 1' },
+  { path: '/route2', content: 'Route 2' }
+]
 
 const Main = () => (
-  <h1>Main</h1>
+  <Fragment>
+    <h1>Main</h1>
+
+    <Switch>
+      {routes.map(route => (
+        <Route
+          key={route.path}
+          path={route.path}
+          render={() => <h2>{route.content}</h2>}
+        />
+      ))}
+    </Switch>
+  </Fragment>
 )
 
 export default Main
