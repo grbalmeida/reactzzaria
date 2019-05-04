9ab46a700f40a69c78c177254fa3d149b92f6cd5
gilvan <g.r.almeida@live.com>
2019-05-03 21:53:56 -0300

    Adding content from the home page

--- order-system/src/pages/main/main.js
+++ order-system/src/pages/main/main.js
@@ -1,6 +1,7 @@
 import React, { useState, useContext } from 'react'
 import {
   AppBar,
+  Grid,
   Toolbar as MaterialToolbar,
   IconButton,
   Typography,
@@ -15,6 +16,7 @@ import { ReactComponent as MainLogo } from 'images/logo.svg'
 const Main = () => {
   const [anchorElement, setAnchorElement] = useState(null)
   const { userInfo, logout } = useContext(AuthContext)
+  const userName = userInfo.user.displayName.split(' ')[0]
 
   const handleOpenMenu = (e) => {
     setAnchorElement(e.target)
@@ -25,24 +27,35 @@ const Main = () => {
   }
 
   return (
-    <AppBar>
-      <Toolbar>
-        <LogoContainer>
-          <Logo />
-        </LogoContainer>
-        <Typography color='inherit'>Hello {userInfo.user.displayName.split(' ')[0]} =)</Typography>
-        <IconButton color='inherit' onClick={handleOpenMenu}>
-          <AccountCircle />
-        </IconButton>
-        <Menu
-          open={!!anchorElement}
-          onClose={handleClose}
-          anchorEl={anchorElement}
-        >
-          <MenuItem onClick={logout}>Logout</MenuItem>
-        </Menu>
-      </Toolbar>
-    </AppBar>
+    <>
+      <AppBar>
+        <Toolbar>
+          <LogoContainer>
+            <Logo />
+          </LogoContainer>
+          <Typography color='inherit'>Hello {userName} =)</Typography>
+          <IconButton color='inherit' onClick={handleOpenMenu}>
+            <AccountCircle />
+          </IconButton>
+          <Menu
+            open={!!anchorElement}
+            onClose={handleClose}
+            anchorEl={anchorElement}
+          >
+            <MenuItem onClick={logout}>Logout</MenuItem>
+          </Menu>
+        </Toolbar>
+      </AppBar>
+      <Content>
+        <Grid container justify='center'>
+          <Grid item>
+            <Typography variant='h3'>
+              O que vai ser hoje, {userName}? =)
+            </Typography>
+          </Grid>
+        </Grid>
+      </Content>
+    </>
   )
 }
 
@@ -67,6 +80,10 @@ const Logo = styled(MainLogo)`
   & line {
     stroke: #fff;
   }
+`
+
+const Content = styled.main`
+  padding: 80px 20px 20px;
 `
 
 export default Main
