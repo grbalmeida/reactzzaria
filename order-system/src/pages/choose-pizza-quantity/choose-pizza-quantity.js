50df2c76e354814b5d4adb264e5f464ff76a5dc0
root <g.r.almeida@live.com>
2019-08-02 20:06:08 -0300

    Adding initial markup to the choose pizza quantity page

--- order-system/src/pages/choose-pizza-quantity/choose-pizza-quantity.js
+++ order-system/src/pages/choose-pizza-quantity/choose-pizza-quantity.js
@@ -1,7 +1,46 @@
 import React from 'react'
+import styled from 'styled-components'
+import { Input as MaterialInput } from '@material-ui/core'
+import {
+  Content,
+  HeaderContent,
+  H4
+} from 'ui'
 
 function ChoosePizzaQuantity () {
-  return <h1>Quantity</h1>
+  return (
+    <Content>
+      <HeaderContent>
+        <H4>
+          How many pizzas would you like<br />
+          to order with these flavors?
+        </H4>
+      </HeaderContent>
+      <MainContent>
+        <Input
+          defaultValue='1'
+          autoFocus
+        />
+      </MainContent>
+    </Content>
+  )
 }
+
+const Input = styled(MaterialInput).attrs({
+  type: 'number'
+})`
+  & input {
+    font-size: 80px;
+    padding: 10px;
+    text-align: center;
+    width: 150px;
+  }
+`
+
+const MainContent = styled.div`
+  display: flex;
+  justify-content: center;
+  margin-top: ${({ theme }) => theme.spacing(2)}px;
+`
 
 export default ChoosePizzaQuantity
