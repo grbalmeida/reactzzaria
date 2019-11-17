5fc95657e751e83e3114d593c19bcc9744eeddbd
root <g.r.almeida@live.com>
2019-11-17 03:47:29 -0300

    Header to checkout confirmation page

--- order-system/src/pages/checkout-confirmation/checkout-confirmation.js
+++ order-system/src/pages/checkout-confirmation/checkout-confirmation.js
@@ -1,10 +1,30 @@
 import React from 'react'
-import { Content } from 'ui'
+import styled from 'styled-components'
+import { Typography } from '@material-ui/core'
+import { Content, H4 } from 'ui'
+import { useAuth } from 'hooks'
 
 function CheckoutConfirmation () {
+  const { userInfo } = useAuth()
+
   return (
-    <Content>Checkout confirmation</Content>
+    <Content>
+      <Header>
+        <H4>Hi {userInfo.user.firstName}</H4>
+        <Typography>
+          Please check if everything is OK with your order before finalizing
+        </Typography>
+      </Header>
+    </Content>
   )
 }
+
+const Header = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  margin-bottom: ${({ theme }) => theme.spacing(3)}px;
+  text-align: center;
+`
 
 export default CheckoutConfirmation
