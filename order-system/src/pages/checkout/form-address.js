999447b999757d59162f71d3d2baa2501d6e5609
gilvan <g.r.almeida@live.com>
2019-12-25 22:11:55 -0300

    State for zip code field with mask

--- order-system/src/pages/checkout/form-address.js
+++ order-system/src/pages/checkout/form-address.js
@@ -1,11 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Grid } from '@material-ui/core'
 import TextField from './text-field'
 
 function FormAddress () {
+  const [cep, setCep] = useState('')
+
+  function handleChangeCep (e) {
+    setCep(cepMask(e.target.value))
+  }
+
+  function cepMask (value) {
+    return value
+      .replace(/\D+/g, '')
+      .replace(/(\d{5})(\d)/, '$1-$2')
+      .replace(/(-\d{3})\d+?/, '$1')
+  }
+
   return (
     <Grid container spacing={2}>
-      <TextField label='Zip code' xs={4} autoFocus />
+      <TextField
+        label='Zip code'
+        xs={4}
+        autoFocus
+        value={cep}
+        onChange={handleChangeCep}
+      />
       <Grid item xs={8} />
 
       <TextField label='Street' xs={9} />
